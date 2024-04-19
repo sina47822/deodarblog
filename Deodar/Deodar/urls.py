fdd38c8701df5efc3778b0f2906c240cfca42cd0
sina <sina>
2024-04-20 03:07:16 +0330

    add first static files and template

--- Deodar/Deodar/urls.py
+++ Deodar/Deodar/urls.py
@@ -14,8 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+# from django.contrib.staticfiles import views
+# from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('blog/', include('blog.urls')),
+    path('', include('core.urls')),
+
+]  
+
+
+if settings.DEBUG:
+    urlpatterns += [
+
+        # re_path(r'^static/(?P<path>.*)$', views.serve),
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+
+    ]
